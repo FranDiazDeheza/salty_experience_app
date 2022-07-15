8e51b16a25aaa64d5bc47906e3cf329b73ecd59b
FranDiazDeheza <fdiazdeheza@gmail.com>
2022-07-15 03:55:07 -0300

    2do

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+    <Navbar/>
+    <div className='gfg'/>
+
+    
+    
+   
+    </Router>
+  );
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,52 @@
 import React from 'react'
-import styled from 'styled-components'
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+ } from './NavbarElements';
+ import svg from '../images/logo.svg';
+ import cart from "../images/cart.svg";
 
-function Navbar() {
+const Navbar = () => {
   return (
     <>
-    <NavContainer>
-        
+    <Nav>
+      <NavLink to="/">
+        <img src={svg} width="100" height="100" alt='logo' />
+      </NavLink>
+      <Bars />
+      <NavMenu>
+        <NavLink to="/about" activesStyle> 
+        About
+        </NavLink>
+        <NavLink to="/rent" activesStyle> 
+        Rent
+        </NavLink>
+        <NavLink to="/Merch" activesStyle> 
+        Merch
+        </NavLink>
+        <NavLink to="/Location" activesStyle> 
+        Where to find us
+        </NavLink>
+        <NavLink to="/ViewCarrito" role="button" className="relative flex">
+        <img src={cart} width="25" height="25" alt='cart' />
+    
+        </NavLink>
+        </NavMenu>
 
 
 
-    </NavContainer>
-    
-    
-    
+
+    </Nav>
+
+
+
+
+
+
     </>
-  
   )
 }
 
 export default Navbar
-
-const NavContainer = styled.nav
